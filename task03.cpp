6d73eaaa16c6cfb317e9e72ff2e6287065b5ff08
Евгений Шкумат <kotbmasle@gmail.com>
2024-04-12 23:46:54 +0300

    task03 done

--- task03.cpp
+++ task03.cpp
@@ -30,5 +30,6 @@
 */
 
 bool task03(int x1, int y1, int x2, int y2) {
-	return false;
+	return (abs(x1 - x2) == 1 && abs(y1 - y2) == 2 || abs(x1 - x2) == 2 && abs(y1 - y2) == 1) &&
+		!(x1 > 8 || x2 > 8 || y1 > 8 || y2 > 8 || x1 < 1 || x2 < 1 || y1 < 1 || y2 < 1 || x2 == x1);
 }
